728729fbcf6ad591fa2b9455bcfccd8485c300f1
Emm4nuell <eduardo.eesf@gmail.om>
2023-08-30 22:41:52 -0300

    Metodo DELETE para Tecnico implementado

--- src/main/java/br/com/Chamado/service/TecnicoService.java
+++ src/main/java/br/com/Chamado/service/TecnicoService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.Chamado.domain.Tecnico;
@@ -45,5 +46,17 @@ public class TecnicoService {
 		}
 		tec.setId(id);
 		return repository.save(tec);
+	}
+
+
+	public void delete(Long id) {
+		Tecnico obj = findById(id);
+		
+		if(obj.getChamados().size() > 0) {
+			System.out.println("Verdadeiro::::" + obj.getChamados());
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}else {			
+			repository.deleteById(id);
+		}
 	}
 }
